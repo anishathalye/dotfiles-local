e73a518626d54bb4f9be0ec4129f826e264c792f
Anish Athalye <me@anishathalye.com>
2017-05-03 15:05:23 -0400

    Add menu bar item to toggle between mono audio

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,4 @@
 require('settings')
 require('reload')
 require('window')
+require('util')
---
+++ hammerspoon/util.lua
@@ -0,0 +1,67 @@
+utilMenu = hs.menubar.new()
+
+local icon = [[
+1 . . . . . . . . . . . 3
+. # # . . . . . . . # # .
+. # # # # . . . # # # # .
+. . # # # # 2 # # # # . .
+. . # # # # # # # # # . .
+. . . # # # # # # # . . .
+. . . 8 # # # # # 4 . . .
+. . . # # # # # # # . . .
+. . # # # # # # # # # . .
+. . # # # # 6 # # # # . .
+. # # # # . . . # # # # .
+. # # . . . . . . . # # .
+7 . . . . . . . . . . . 5
+]]
+
+utilMenu:setIcon('ASCII:' .. icon)
+
+local menu = nil
+
+local reloadMenu = function() utilMenu:setMenu(menu) end
+
+menu = {
+  {
+    title = "Mono audio",
+    checked = false,
+    fn = function(modifiers, menuItem)
+      local script = [[
+        tell application "System Preferences"
+          reveal anchor "Hearing" of pane id "com.apple.preference.universalaccess"
+        end tell
+
+        tell application "System Events"
+          tell application process "System Preferences"
+            set frontmost to true
+            tell window "Accessibility"
+              activate
+              repeat until checkbox "Play stereo audio as mono" exists
+                delay 0.05
+              end repeat
+              set monoStereoCheckbox to checkbox "Play stereo audio as mono"
+              tell monoStereoCheckbox
+                if (%s its value as boolean) then click monoStereoCheckbox
+              end tell
+            end tell
+          end tell
+        end tell
+
+        tell application "System Preferences" to quit
+      ]]
+
+      local toggle = ""
+      if not menuItem.checked then
+        toggle = "not"
+      end
+      script = string.format(script, toggle)
+      hs.osascript.applescript(script)
+
+      menuItem.checked = not menuItem.checked
+      reloadMenu()
+    end
+  }
+}
+
+reloadMenu()
