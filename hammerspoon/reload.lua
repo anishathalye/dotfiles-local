829ad96358902dde4c6918d7906890c4a833cc88
Anish Athalye <me@anishathalye.com>
2017-06-13 09:35:41 -0700

    Improve config reloading

--- hammerspoon/reload.lua
+++ hammerspoon/reload.lua
@@ -1,6 +1,11 @@
-function reload()
-  hs.notify.new({title='Reloading', informativeText='Reloading Hammerspoon config'}):send()
-  hs.reload()
+function reload(files)
+  for _, file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      hs.notify.new({title='Reloading', informativeText='Reloading Hammerspoon config'}):send()
+      hs.reload()
+      return
+    end
+  end
 end
 
 reloadWatcher = hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reload):start()
