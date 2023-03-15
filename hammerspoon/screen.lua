fb15a175e2c728cf9fede800201c1fe361a436f6
Anish Athalye <me@anishathalye.com>
2023-03-15 08:28:43 -0400

    Fix

--- hammerspoon/screen.lua
+++ hammerspoon/screen.lua
@@ -1,3 +1,3 @@
-hs.grid.setGrid('5x3', HOME_RIGHT_MONITOR)
-hs.grid.setGrid('2x4', HOME_LEFT_MONITOR)
+hs.grid.setGrid('5x3', LAB_RIGHT_MONITOR)
+hs.grid.setGrid('2x4', LAB_LEFT_MONITOR)
 hs.grid.setGrid('5x3', DORM_MONITOR)
