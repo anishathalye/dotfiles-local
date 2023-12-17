8c3c747a5232489346588ef8956515230091d942
Anish Athalye <me@anishathalye.com>
2023-12-17 12:40:12 -0500

    Add keybindings for media keys on TKL keyboard

--- hammerspoon/shortcut.lua
+++ hammerspoon/shortcut.lua
@@ -13,3 +13,11 @@ hs.hotkey.bind({'shift', 'cmd'}, 'pad6', function() systemKey('NEXT') end)
 hs.hotkey.bind({'shift', 'cmd'}, 'pad8', function() systemKey('SOUND_UP') end)
 hs.hotkey.bind({'shift', 'cmd'}, 'pad2', function() systemKey('SOUND_DOWN') end)
 hs.hotkey.bind({'shift', 'cmd'}, 'pad0', function() systemKey('MUTE') end)
+
+-- simulate media keys for tenkeyless keyboard
+hs.hotkey.bind({'shift', 'cmd'}, 'return', function() systemKey('PLAY') end)
+hs.hotkey.bind({'shift', 'cmd'}, 'left', function() systemKey('PREVIOUS') end)
+hs.hotkey.bind({'shift', 'cmd'}, 'right', function() systemKey('NEXT') end)
+hs.hotkey.bind({'shift', 'cmd'}, 'up', function() systemKey('SOUND_UP') end)
+hs.hotkey.bind({'shift', 'cmd'}, 'down', function() systemKey('SOUND_DOWN') end)
+hs.hotkey.bind({'shift', 'cmd'}, 'm', function() systemKey('MUTE') end)
