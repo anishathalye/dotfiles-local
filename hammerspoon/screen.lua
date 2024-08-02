8c59f6ebc796ccbf43661e7543ba5306829552e3
Anish Athalye <me@anishathalye.com>
2024-08-02 13:42:52 -0400

    Update monitors

--- hammerspoon/screen.lua
+++ hammerspoon/screen.lua
@@ -1,5 +1,2 @@
-hs.grid.setGrid('5x3', LAB_RIGHT_MONITOR)
-hs.grid.setGrid('2x4', LAB_LEFT_MONITOR)
-hs.grid.setGrid('6x4', DORM_MONITOR)
 hs.grid.setGrid('5x3', HOME_MONITOR)
 hs.grid.setGrid('5x3', CLEANLAB_MONITOR)
